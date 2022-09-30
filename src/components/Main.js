a26465e48123523770ca38c1196cfe465e6e1eac
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-30 19:17:26 -0300

    fix: title is already finish now

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,19 +1,40 @@
+import { useContext } from "react";
 import styled from "styled-components"
 import logo from "../styles/images/Ranting.png"
-//import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-//import {faCoffee } from '@fortawesome/fontawesome-free-solid'
-//import {  } from "@fortawesome/fontawesome-free-regular"
-//import {  } from "@fortawesome/fontawesome-svg-core"
+import UserContext from "../contexts/userContext";
+import TokenContext from "../contexts/tokenContext";
+import { useNavigate } from "react-router-dom";
 
 export default function MainScreen() { 
-
+    const { userData } = useContext(UserContext);
+    const { token, setToken } = useContext(TokenContext);
+    const navigate = useNavigate();
+    console.log(userData);
+    console.log(token);
+    setToken("")
     return(
         <Container>
             <Title>
                 <span><ion-icon name="search-sharp"></ion-icon> Search</span>
                 <img src={logo} alt="logo"/>
-                <UserProfile><span>Olá, son</span></UserProfile>
+                {token ? (
+                <UserProfile>
+                    <span>Olá, son</span>
+                    <img src="https://s.yimg.com/uu/api/res/1.2/yJgQLWYXVhVDgw.PFwCQ.g--~B/aD01MjE7dz03Njg7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/pt/afp.com.br/6eab9e01093eb739a7a12cfecd85b6b0" alt="profile"/>
+                </UserProfile>
+                ): (
+                    <Sign>
+                        <button id="sign-up" onClick={() => navigate("/sign-up")}>Sign-up</button>
+                        <button id="login" onClick={() => navigate("/login")}>Login</button>
+                    </Sign>
+                )}
             </Title>
+
+            <Main>
+                <ul>
+                    
+                </ul>
+            </Main>
         </Container>
     )
 }
@@ -66,5 +87,53 @@ const Title = styled.div`
 const UserProfile = styled.div`
     display: flex;
 
+    img { 
+        width: 50px;
+        height: 50px;
+        object-fit: cover;
+        border-radius: 50%;
+        margin-left: 5px;
+    }
+`
+const Sign = styled.div`
+    display: flex;
+
+    button { 
+        width: 70px;
+        height: 40px;
+        margin-right: 10px;
+        border: 1px solid #359FE4;
+        border-radius: 15px;
+        display: flex; 
+        align-items: center; 
+        justify-content: center;
+        font-weight: bold;
+        font-size: 16px;
+
+        &:hover { 
+            cursor: pointer;
+        }
     
+        &:active {  
+            transform: scale(0.98);
+            box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
+        }
+    }
+
+    button#login { 
+        background-color: black; 
+        color: white; 
+    } 
+
+    button#sign-up { 
+        background-color: white; 
+        color: black; 
+    }
+`
+const Main = styled.div`
+    width: 100%; 
+    height: 100%; 
+    display: flex; 
+    align-items: center; 
+    flex-direction: row;
 `
