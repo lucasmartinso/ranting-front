93d5dad07cc34da477ed79601f4e7b22e6bf409f
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-30 11:24:28 -0300

    feat: create axios login request

---
+++ src/components/Login.js
@@ -0,0 +1,236 @@
+import { useState } from "react"
+import styled from "styled-components"
+import logo from "../styles/images/Ranting.png"
+import { ThreeDots } from "react-loader-spinner"
+import { useNavigate } from "react-router-dom";
+import * as AxiosRequest from "../repositories/AxiosRequests"
+
+export default function SignUpScreen() { 
+  const [name,setName] = useState("");
+  const [email,setEmail] = useState("");
+  const [username,setUsername] = useState("");
+  const [password,setPassword] = useState("");
+  const [confirmPassword,setConfirmPassword] = useState("");
+  const [clicked,setClicked] = useState(false);
+  const [error,setError] = useState(false);
+  const [errorMessage, setErrorMessage] = useState("");
+  const navigate = useNavigate();
+
+  async function register(event) { 
+    event.preventDefault();
+
+    const userData = { 
+      name,
+      username,
+      email,
+      password,
+      confirmPassword
+    }
+
+    try {
+      setClicked(true);
+      setConfirmPassword("");
+      setPassword("");  
+      await AxiosRequest.login(userData);
+      navigate("/login");
+    } catch (error) {
+      console.log(error);
+      setErrorMessage(error.response.data);
+      setClicked(false);
+      setError(true);
+    }
+  }
+
+  return(
+    <Container>
+
+      <Title>
+        <img src={logo} alt="logo"/>
+      </Title>
+
+      <form onSubmit={register}>
+      <Main error={error}>
+        <input
+            type="text"
+            placeholder="Name"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            required
+        />
+        <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            required
+        />
+         <input
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+            required
+        />
+        <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            required
+        />
+        <input
+            type="password"
+            placeholder="Confirm Password"
+            value={confirmPassword}
+            onChange={(event) => setConfirmPassword(event.target.value)}
+            required
+        />
+        <button>
+          {clicked ? (
+            <ThreeDots color="white" height={80} width={100} />
+          ) : ("Register")}
+        </button>
+      </Main>
+      </form>
+      
+      {error ? (
+      <Error>
+          <button>
+            <span>{errorMessage}</span>
+            <span id="x" onClick={() => setError(false)}>X</span>
+          </button>
+      </Error>
+      ) : ""}
+
+      <Message onClick={() => navigate("/login")}>
+        <span>Already registred? Sign-in!</span>
+      </Message>
+    </Container>
+  )
+}
+
+const Container = styled.div`
+  width: 100%; 
+  height: 100%; 
+  display: flex; 
+  flex-direction: column;
+`
+const Title = styled.div`
+  width: 100%; 
+  height: 100%;
+  display: flex; 
+  justify-content: center;
+  margin-top: 50px;
+
+  img { 
+    width: 500px;
+    height: 300px;
+  }
+`
+const Main = styled.div`
+  width: 100%; 
+  height: 100%; 
+  display: flex; 
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: ${props => props.error ? ("25px") : ("35px")};
+
+  input { 
+    width: 80%; 
+    height: 70px;
+    display: flex;
+    align-items: center;
+    border: 2px solid rgba(120, 177, 89, 0.25);
+    box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 10px;
+    padding-left: 20px;
+    font-size: 20px;
+    font-color: #D5D5D5;
+    font-color: rgba(0, 0, 0, 1);
+    margin-bottom: 25px;
+  }
+
+  button { 
+    width: 80%; 
+    height: 70px; 
+    display: flex;
+    align-items: center; 
+    justify-content: center;
+    background-color: #000000;
+    color: rgba(255,255,255,1);
+    font-size: 30px;
+    font-weight: bold;
+    border: 2px solid rgba(120, 177, 89, 0.25);
+    box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
+    transition: 0.2s all;
+
+    &:hover { 
+      cursor: pointer;
+    }
+
+    &:active {  
+      transform: scale(0.98);
+      box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
+    }
+  }
+`
+const Error = styled.div` 
+  width: 100%; 
+  height: 100%; 
+  display: flex; 
+  justify-content: center; 
+
+  button { 
+    width: 80%; 
+    height: 70px; 
+    display: flex;
+    align-items: center; 
+    justify-content: space-between;
+    padding: 0px 20px 0px 20px;
+    background-color: #FF7474;
+    color: rgba(255,255,255,1);
+    font-size: 20px;
+    font-weight: bold;
+    border: 2px solid rgba(120, 177, 89, 0.25);
+    box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.25);
+    border-radius: 4px;
+    transition: 0.2s all;
+
+   span#x { 
+    &:hover { 
+      cursor: pointer;
+    }
+
+    &:active {  
+      transform: scale(0.98);
+      box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
+    }
+   }
+  }
+`
+const Message = styled.div`
+  width: 100%; 
+  height: 100%; 
+  display: flex; 
+  justify-content: center; 
+
+  span {
+    color: white; 
+    font-weight: bold;
+    font-size: 20px;
+    text-decoration: underline;
+    text-decoration-color: #359FE4;
+    margin-bottom: 70px;
+    margin-top: 30px;
+
+    &:hover{ 
+      cursor: pointer; 
+    }
+  
+    &:active {  
+      transform: scale(0.98);
+      box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
+    }
+  }
+`
--- src/repositories/AxiosRequests.js
+++ src/repositories/AxiosRequests.js
@@ -5,3 +5,7 @@ const URL = "http://localhost:5000"
 export async function signup(userData) { 
     await axios.post(`${URL}/sign-up`,userData);
 }
+
+export async function login(userData) { 
+    await axios.post(`${URL}/login`,userData);
+}
