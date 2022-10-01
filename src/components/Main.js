c5a7bf8cd9accc0a59c0bb27c737b99a1dc98d25
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-09-30 21:14:24 -0300

    feat: box is is done

--- src/components/Main.js
+++ src/components/Main.js
@@ -32,14 +32,41 @@ export default function MainScreen() {
             <Main>
                 <ul>
                     <Place>
-                        <Overall>
-                            <span>Paris 6</span>
-                            <h3>4,7 ⭐</h3>
-                        </Overall>
-                        <Info>
-                            <img src="https://s.yimg.com/uu/api/res/1.2/yJgQLWYXVhVDgw.PFwCQ.g--~B/aD01MjE7dz03Njg7YXBwaWQ9eXRhY2h5b24-/https://media.zenfs.com/pt/afp.com.br/6eab9e01093eb739a7a12cfecd85b6b0" alt="place"/>
-                        </Info>
-                                                
+                        <Container2>
+                            <Overall>
+                                <span>Paris 6 <ion-icon name="checkmark-circle"></ion-icon></span>
+                                <h3>4,7 ⭐</h3>
+                            </Overall>
+                            <Info>
+                                <img src="https://i0.wp.com/sarapateando.com.br/wp-content/uploads/2021/05/47b15ddd-6b2b-4926-a3b7-57bb9e08abda.jpg?fit=1280%2C960&ssl=1" alt="place"/>
+                            </Info>
+                        </Container2>
+                        <Ranting>
+                            <TextBox>
+                                <span>Carro chefe:</span>
+                                <h4>Salmão</h4>
+                            </TextBox> 
+                            <TextBox>
+                                <span>N de avaliações:</span>
+                                <h4>372</h4>
+                            </TextBox>
+                            <TextBox>
+                                <span>Comida:</span>
+                                <h4> 4,8 ⭐</h4>
+                            </TextBox>
+                            <TextBox>
+                                <span>Atendimento:</span>
+                                <h4> 4,8 ⭐</h4>
+                            </TextBox>
+                            <TextBox>
+                                <span>Preço:</span>
+                                <h4> 4,8 ⭐</h4>
+                            </TextBox>
+                            <TextBox>
+                                <span>Ambiente:</span>
+                                <h4> 4,8 ⭐</h4>
+                            </TextBox>
+                        </Ranting>                 
                     </Place>
                 </ul>
             </Main>
@@ -141,6 +168,7 @@ const Sign = styled.div`
 const Main = styled.div`
     width: 100%; 
     height: 100%; 
+    margin-top: 150px;
 
     ul { 
         width: 100%; 
@@ -154,11 +182,9 @@ const Place = styled.li`
     width: 90%;
     height: 350px;
     background-color: white;
-    margin-top: 150px;
     border-radius: 12px;
     padding: 25px 0px 0px 0px;
     box-shadow: 4px 4px 4px 4px rgba(0, 0, 0, 0.25);
-    display: flex;
 
     &:hover { 
         cursor: pointer;
@@ -168,31 +194,65 @@ const Place = styled.li`
         transform: scale(0.98);
         box-shadow: 3px 2px 22px 1px rgba(0, 0, 0, 0.24);
     }
-    
+`
+const Container2 = styled.div`
+    width: 100%; 
+    height: 60%; 
+    display: flex;
 `
 const Overall = styled.div`
-    width: 12%; 
+    width: 15%; 
     height: 100%; 
     display: flex;
     flex-direction: column;
     align-items: center;
-    font-size: 24px;
+    font-size: 23px;
     font-weight: bold;
     margin-top: 20px;
+    margin-right: 10px;
+    font-family: 'Playball', cursive;
 
     span { 
-        margin-bottom: 10px;
+        margin-bottom: 20px;
+        text-decoration: overline;
+        display: flex;
+
+        ion-icon { 
+            color: #3797F0;
+            margin-left: 8px;
+            transition: 0.2s all;
+        }
+    }
+
+    h3 { 
+        font-weight: black;
     }
 `
 const Info = styled.div`
-    width: 88%; 
+    width: 85%; 
     height: 100%;
     display: flex;
 
     img { 
-        width: 97%;
-        height: 65%;
+        width: 98%;
+        height: 90%;
         object-fit: cover;
         border-radius: 6px;
+    }
+` 
+const Ranting = styled.div`
+    width: 100%; 
+    display: flex;
+    justify-content: center;
+    align-items: space-between;
+    font-family: 'Playball', cursive;
+    font-size: 24px;
+`
+const TextBox = styled.li`
+    text-align: center;
+    margin-right: 20px;
+
+    span {
+        font-weight: 500;
     }
 `
