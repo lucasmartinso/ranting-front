f14c763fc292f172b49b47e97557345bfa353847
Lucas Martins <lucasmartinsoliveiraa@gmail.com>
2022-12-01 10:41:31 -0300

    config: app to recive filter context

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import SignUpScreen from "./components/Signup";
 import GlobalStyle from "./styles/globalStyles";
 import LoginScreen from "./components/Login";
 import TokenContext from"./contexts/tokenContext";
-import UserContext from "./contexts/userContext"
+import UserContext from "./contexts/userContext";
 import AuthContext from "./contexts/authContext";
+import FilterContext from "./contexts/filterContext";
 import MainScreen from "./components/Main";
 import PlaceScreen from "./components/Place";
 import CreatePlaceScreen from "./components/CreatePlace";
@@ -16,11 +17,13 @@ export default function App() {
   const [token, setToken] = useState(localStorage.getItem("MY_TOKEN"));
   const [userData, setUserData] = useState(localStorage.getItem("USER_DATA"));
   const [auth, setAuth] = useState(false);
+  const [filterPlaces, setFilterPlaces] = useState([]);
 
   return (
     <TokenContext.Provider value={{token,setToken}}>
     <UserContext.Provider value={{userData,setUserData}}>
     <AuthContext.Provider value={{auth,setAuth}}>
+    <FilterContext.Provider value={{filterPlaces,setFilterPlaces}}>
       <GlobalStyle />
       <BrowserRouter>
           <Routes>
@@ -32,6 +35,7 @@ export default function App() {
               <Route path="/create/place" element={<CreatePlaceScreen />} />
           </Routes>
       </BrowserRouter>
+    </FilterContext.Provider>
     </AuthContext.Provider>
     </UserContext.Provider>
     </TokenContext.Provider>
